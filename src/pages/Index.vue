ddd4e1c687e3e62aaab7f63b60007e28b1485406
Tommy Alhamra <tommy.alhamra@gmail.com>
2021-01-04 19:03:23 +0700

    add blog with gridsome

---
+++ src/pages/Index.vue
@@ -0,0 +1,48 @@
+<template>
+  <Layout>
+    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
+    <g-image alt="Example image" src="~/favicon.png" width="135" />
+
+    <h1>Hello, MantraCinta!</h1>
+
+    <p>
+      mantracinta is Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      Quisquam numquam vel accusantium nobis eum officia dolores ducimus
+      cupiditate harum earum illo necessitatibus vitae labore at fugit autem
+      minima, perspiciatis nam perferendis aliquam? Saepe dolorum tenetur
+      delectus nihil? Ullam perferendis non dignissimos aliquid suscipit dolorem
+      exercitationem neque soluta reiciendis omnis tempore veniam totam, illum,
+      magnam est recusandae architecto sunt fuga maiores asperiores illo!
+      Nostrum explicabo modi expedita, commodi magnam quas animi iste molestias
+      minima! Quasi dolorem dolore fuga saepe et quam, cum explicabo natus quia
+      vel velit magni, sed unde ex optio impedit? Quisquam repellat eligendi
+      molestias vero ullam fugiat? Porro.
+    </p>
+
+    <p class="home-links">
+      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener"
+        >Gridsome Docs</a
+      >
+      <a
+        href="https://github.com/gridsome/gridsome"
+        target="_blank"
+        rel="noopener"
+        >GitHub</a
+      >
+    </p>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Hello, world!",
+  },
+};
+</script>
+
+<style>
+.home-links a {
+  margin-right: 1rem;
+}
+</style>
