21ec2d44899f1c8b80b8dffe57754886bd57421e
Tommy Alhamra <tommy.alhamra@gmail.com>
2021-12-09 15:30:18 +0700

    update content coming soon

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# website.online
+# EpicDay
 
 ## Build Setup
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,71 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        website.online
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div>
+    <v-container>
+      <v-row align="center" justify-center>
+        <v-col cols="12">
+          <v-card max-width="344">
+            <v-img
+              src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+              height="200px"
+            >
+            </v-img>
+
+            <v-card-title>
+              Coming Soon
+            </v-card-title>
+            <v-card-subtitle>
+              Kabar Gembira untuk orang Tercinta. Sebarkan Hari Paling Epic Anda
+              dengan Undangan Online
+            </v-card-subtitle>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+
+              <a target="_blank"
+                href="https://www.instagram.com/undanganepic/"
+                style="text-decoration: none;"
+              >
+                <v-btn large fab icon>
+                  <v-icon>mdi-instagram</v-icon>
+                </v-btn>
+              </a>
+
+              <!-- <nuxt-link large icon> </nuxt-link> -->
+            </v-card-actions>
+
+            <v-card-actions @click="show = !show">
+              <v-btn color="orange lighten-2" text>
+                Promo Murmer
+              </v-btn>
+
+              <v-spacer></v-spacer>
+
+              <v-btn icon>
+                <v-icon>{{
+                  show ? "mdi-chevron-up" : "mdi-chevron-down"
+                }}</v-icon>
+              </v-btn>
+            </v-card-actions>
+
+            <v-expand-transition>
+              <div v-show="show">
+                <v-divider></v-divider>
+
+                <v-card-text> Butuh Cepet dan Murah ? <b>Dm</b> </v-card-text>
+              </div>
+            </v-expand-transition>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data: () => ({
+    show: false
+  })
+};
 </script>
 
 <style>
@@ -42,16 +79,8 @@ export default {}
 }
 
 .title {
-  font-family:
-    'Quicksand',
-    'Source Sans Pro',
-    -apple-system,
-    BlinkMacSystemFont,
-    'Segoe UI',
-    Roboto,
-    'Helvetica Neue',
-    Arial,
-    sans-serif;
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
   display: block;
   font-weight: 300;
   font-size: 100px;
