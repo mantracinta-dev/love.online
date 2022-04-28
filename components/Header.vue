ce7ad69ca8d9341ee32c9847b8064c45dff0b661
Tommy Alhamra <tommy@modoc.id>
2022-04-28 22:46:37 +0700

    fix: header

--- components/Header.vue
+++ components/Header.vue
@@ -3,20 +3,44 @@
     <v-container>
       <v-row align="center">
         <v-col cols="6">
-          <h1>Kabar Gembira untuk orang tercinta</h1>
-          <p>
-            Kabar Gembira untuk orang tercinta. Sebarkan hari paling epic anda
-            dengan undangan online
+          <h1 class="text-4xl font-weight-bold mb-3 color-text--blue line-6px">
+            Sebarkan Kabar Bahagiamu.
+          </h1>
+          <p class="text-4xl font-weight-bold text--secondary mb-6">
+            Murah, Mudah & Mewah
           </p>
-          <v-btn> Yuk Buat </v-btn>
+          <p class="mb-6 text--primary">
+            Kabar Gembira untuk orang tercinta. Sebarkan kabar bahagiamu
+            keseluruh teman dan kerabat dengan sentuhan teknologi. Bersama
+            EpicDay yang membuat undangan kamu jauh lebih Epic dari yang
+            lainnya.
+          </p>
+          <v-btn
+            rounded
+            dark
+            class="font-weight-bold text-capitalize text-base color-bg--red"
+          >
+            Yuk Buat Sekarang
+          </v-btn>
         </v-col>
         <v-col cols="6">
           <div class="img-ab-header">
             <v-img
-              lazy-src="https://picsum.photos/id/11/10/6"
               :src="require('../assets/img/img-main.png')"
-              alt="image"
-            ></v-img>
+              lazy-src="https://picsum.photos/id/1013/4256/2832"
+              max-height="600"
+              max-width="100%"
+              alt="image-unsplash-epicday"
+            >
+              <template v-slot:placeholder>
+                <v-row class="fill-height ma-0" align="center" justify="center">
+                  <v-progress-circular
+                    indeterminate
+                    color="grey lighten-5"
+                  ></v-progress-circular>
+                </v-row>
+              </template>
+            </v-img>
           </div>
         </v-col>
       </v-row>
