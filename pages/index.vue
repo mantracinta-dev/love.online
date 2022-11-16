bc8e9a6c8a104d3ebe98fb5396e3207aade4de66
Tommy Alhamra <tommy@modoc.id>
2022-11-16 23:43:09 +0700

    add meta

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <Navbar />
     <Header />
     <Feature />
-    <Product />
+    <!-- <Product /> -->
     <!-- <HowToOrder />
     <Faq />
     <Cta />
@@ -14,6 +14,36 @@
 <script>
 export default {
   name: "IndexPage",
+  head: {
+    title: process.env.TITLE,
+    meta: [
+      {
+        hid: "og:title",
+        property: "og:title",
+        content: "Epicday | Undangan Online",
+      },
+      { hid: "og:type", property: "og:type", content: "product" },
+      { hid: "og:site_name", property: "og:site_name", content: "Epicday" },
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "Buat Undangan Mewah dan murah bukan lagi persoalan hanya di Epicday",
+      },
+      {
+        hid: "og:description",
+        name: "og:description",
+        content:
+          "Buat Undangan Mewah dan murah bukan lagi persoalan hanya di Epicday",
+      },
+      { hid: "og:url", name: "og:url", content: process.env.BASE_URL },
+      {
+        hid: "og:image",
+        name: "og:image",
+        content: "../static/logo.png",
+      },
+    ],
+  },
   data() {
     return {};
   },
